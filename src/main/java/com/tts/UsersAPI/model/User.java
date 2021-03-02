d848872434364b3d6bdc12a3359eb2d15919a9cb
TJGit11 <74613952+TJGit11@users.noreply.github.com>
2021-03-02 16:13:06 -0600

    first commit

---
+++ src/main/java/com/tts/UsersAPI/model/User.java
@@ -0,0 +1,26 @@
+package com.tts.UsersAPI.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String id;
+    private String firstName;
+    private String lastName;
+    private String state;
+}
